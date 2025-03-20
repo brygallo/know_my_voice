8e50e55b1ced90bc395f0cdb2f0a242ac8769946
bryan13gallo <bryan13gallo@gmail.com>
2025-03-20 10:44:11 -0500

    Refactor Docker setup and streamline deployment process
    Updated Dockerfile for clearer comments and streamlined instructions. Improved database URL handling in docker-compose. Refined GitLab CI/CD pipeline by replacing manual Docker commands with docker-compose and enhancing entrypoint logic for PostgreSQL readiness.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-echo "Esperando a que PostgreSQL esté listo..."
-while ! nc -z db 5432; do
-  sleep 1
+
+echo "Waiting for PostgreSQL to be ready..."
+until PGPASSWORD=$DATABASE_PASSWORD psql -h "db" -U "$DATABASE_USER" -d "$DATABASE_NAME" -c '\q'; do
+  >&2 echo "PostgreSQL is still unavailable - sleeping"
+  sleep 2
 done
-echo "PostgreSQL está listo."
 
-echo "Aplicando migraciones..."
-python manage.py migrate
+echo "PostgreSQL is up - applying migrations"
 
-echo "Recopilando archivos estáticos..."
+# Apply migrations
+python manage.py migrate --noinput
+
+# Collect static files
 python manage.py collectstatic --noinput
 
-echo "Iniciando Gunicorn..."
+echo "Starting Gunicorn..."
 exec gunicorn --bind 0.0.0.0:8000 mi_proyecto.wsgi:application
